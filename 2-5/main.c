ae506e1053d83ebb29a43df5cc262bad27073ef8
raskin ivan <raskin.ivan@phystech.edu>
2020-12-06 19:06:01 +0300

    Update main.c

--- 2-5/main.c
+++ 2-5/main.c
@@ -111,95 +111,119 @@ struct Segment_state
 		sec(sec), index(index), open_close(open_close) {}
 };
 
-auto cmp1 = [](std::pair<Segment, int> a, std::pair<Segment, int> b) {
-	long double a_y;
-	long double b_y;
-	long double x = std::max(a.first.left_end_.x_, b.first.left_end_.x_);
-	a_y = a.first.get_y_(x);
-	b_y = b.first.get_y_(x);
-	return ((a_y < b_y) || ((a_y == b_y) && (a.second < b.second)));
-};
 
-auto cmp2 = [](Segment_state a, Segment_state b) {
-	Point A, B;
-	A = a.open_close == 0 ? a.sec.left_end_ : a.sec.right_end;
-	B = b.open_close == 0 ? b.sec.left_end_ : b.sec.right_end;
-	if (A.x_ < B.x_)
-		return true;
-	if (A.x_ == B.x_)
-	{
-		if (a.open_close == 0 && b.open_close == 1)
-			return true;
-		if (a.open_close == b.open_close && a.index < b.index)
-			return true;
+struct cmp_by_y {
+	bool operator()(const std::pair<Segment, int>& a, const std::pair<Segment, int>& b) {
+		long double a_y;
+		long double b_y;
+		long double x = std::max(a.first.left_end_.x_, b.first.left_end_.x_);
+		a_y = a.first.get_y_(x);
+		b_y = b.first.get_y_(x);
+		return ((a_y < b_y) || ((a_y == b_y) && (a.second < b.second)));
 	}
-	return false;
-
 };
 
-bool SegmentCross(Segment first, Segment second)
+struct cmp_by_x {
+	bool operator()(const Segment_state& a, const Segment_state& b) {
+		Point A, B;
+		A = a.open_close == 0 ? a.sec.left_end_ : a.sec.right_end;
+		B = b.open_close == 0 ? b.sec.left_end_ : b.sec.right_end;
+		if (A.x_ < B.x_)
+		{
+			return true;
+		}
+		if (A.x_ == B.x_)
+		{
+			if (a.open_close == 0 && b.open_close == 1)
+			{
+				return true;
+			}
+			if (a.open_close == b.open_close && a.index < b.index)
+			{
+				return true;
+			}
+		}
+		return false;
+	};
+};
+
+bool IsSegmentsCross(Segment first, Segment second)
 {
 	//std::cout << first << ' ' << second << '\n';
 	Vector V1(first.left_end_, first.right_end);
 	Vector V2(first.left_end_, second.left_end_);
 	Vector V3(first.left_end_, second.right_end);
 	if (CrossProduct(V1, V2) * CrossProduct(V1, V3) > 0)
+	{
 		return 0;
+	}
 	V1 = Vector(second.left_end_, second.right_end);
 	V2 = Vector(second.left_end_, first.left_end_);
 	V3 = Vector(second.left_end_, first.right_end);
 	if (CrossProduct(V1, V2) * CrossProduct(V1, V3) > 0)
+	{
 		return 0;
+	}
 	if (CrossProduct(V1, V2) * CrossProduct(V1, V3) == 0
 		&& CrossProduct(V1, V2) * CrossProduct(V1, V3) == 0)
+	{
 		if (std::min(first.left_end_.y_, first.right_end.y_) >
 			std::max(second.left_end_.y_, second.right_end.y_) ||
 			std::min(second.left_end_.y_, second.right_end.y_) >
 			std::max(first.left_end_.y_, first.right_end.y_))
+		{
 			return 0;
+		}
+	}
 	return 1;
 }
 
 std::pair<int, int> AddSegment(int i, std::vector<Segment>& segments, 
-	std::set<std::pair<Segment, int>, decltype(cmp1)>& Segment_sort,
-	std::vector<Segment_state>& skanline)
+	std::set < std::pair<Segment, int>, decltype(cmp_by_y{}) > & Segment_sort,
+	std::vector<Segment_state>& scanline)
 {
-	std::set <std::pair<Segment, int>>::iterator it, it_minus1, it_plus1;
-	Segment_sort.insert(std::make_pair(skanline[i].sec, skanline[i].index));
-	it = Segment_sort.find(std::make_pair(skanline[i].sec, skanline[i].index));
-	it_plus1 = it;
-	it_minus1 = it;
-	++it_plus1;
+	std::set <std::pair<Segment, int>>::iterator it, prev, next;
+	Segment_sort.insert(std::make_pair(scanline[i].sec, scanline[i].index));
+	it = Segment_sort.find(std::make_pair(scanline[i].sec, scanline[i].index));
+	next = it;
+	prev = it;
+	++next;
 
 	if (it != Segment_sort.begin())
 	{
-		--it_minus1;
-		if (SegmentCross(segments[(*it_minus1).second], segments[(*it).second]))
-			return std::make_pair((*it_minus1).second, (*it).second);
+		--prev;
+		if (IsSegmentsCross(segments[(*prev).second], segments[(*it).second]))
+		{
+			return std::make_pair((*prev).second, (*it).second);
+		}
 	}
 
-	if (it_plus1 != Segment_sort.end())
+	if (next != Segment_sort.end())
 	{
-		if (SegmentCross(segments[(*it_plus1).second], segments[(*it).second]))
-			return std::make_pair((*it_plus1).second, (*it).second);
+		if (IsSegmentsCross(segments[(*next).second], segments[(*it).second]))
+		{
+			return std::make_pair((*next).second, (*it).second);
+		}
 	}
 	return std::make_pair(-1, -1);
 }
 
 std::pair<int, int> DeleteSegment(int i, std::vector<Segment>& segments,
-	std::set<std::pair<Segment, int>, decltype(cmp1)>& Segment_sort,
-	std::vector<Segment_state>& skanline)
+	std::set < std::pair<Segment, int>, decltype(cmp_by_y{}) > & Segment_sort,
+	std::vector<Segment_state>& scanline)
 {
-	std::set <std::pair<Segment, int>>::iterator it, it_minus1, it_plus1;
-	it = Segment_sort.find(std::make_pair(skanline[i].sec, skanline[i].index));
-	it_minus1 = it;
-	it_plus1 = it;
-	++it_plus1;
-	if (it != Segment_sort.begin() && it_plus1 != Segment_sort.end())
+	std::set <std::pair<Segment, int>>::iterator it, prev, next;
+	it = Segment_sort.find(std::make_pair(scanline[i].sec, scanline[i].index));
+	prev = it;
+	next = it;
+	++next;
+	if (it != Segment_sort.begin() && next != Segment_sort.end())
 	{
-		--it_minus1;
-		if (SegmentCross(segments[(*it_plus1).second], segments[(*it_minus1).second]))
-			return std::make_pair((*it_plus1).second, (*it_minus1).second);
+		--prev;
+		if (IsSegmentsCross(segments[(*next).second], segments[(*prev).second]))
+		{
+			return std::make_pair((*next).second, (*prev).second);
+		}
 	}
 	Segment_sort.erase(it);
 	return std::make_pair(-1, -1);
@@ -208,30 +232,34 @@ std::pair<int, int> DeleteSegment(int i, std::vector<Segment>& segments,
 std::pair<int, int> FindCrossSegments(std::vector<Segment>& segments)
 {
 
-	std::set<std::pair<Segment, int>, decltype(cmp1)> Segment_sort(cmp1);
-	std::set <std::pair<Segment, int>>::iterator it, it_minus1, it_plus1;
-	std::vector<Segment_state> skanline;
+	std::set < std::pair<Segment, int>, decltype(cmp_by_y{}) > Segment_sort(cmp_by_y{});
+	std::set <std::pair<Segment, int>>::iterator it, prev, next;
+	std::vector<Segment_state> scanline;
 	for (int64_t i = 0; i < segments.size(); ++i)
 	{
-		skanline.push_back(Segment_state(segments[i], i, 0));
-     	skanline.push_back(Segment_state(segments[i], i, 1));
+		scanline.push_back(Segment_state(segments[i], i, 0));
+     	scanline.push_back(Segment_state(segments[i], i, 1));
 	}
-	std::sort(skanline.begin(), skanline.end(), cmp2);
-	for (int64_t i = 0; i < skanline.size(); ++i)
+	std::sort(scanline.begin(), scanline.end(), cmp_by_x{});
+	for (int64_t i = 0; i < scanline.size(); ++i)
 	{
-		if (skanline[i].open_close == 0)
+		if (scanline[i].open_close == 0)
 		{
 			std::pair<int, int> crossing_sections =
-				AddSegment(i, segments, Segment_sort, skanline);
+				AddSegment(i, segments, Segment_sort, scanline);
 			if (crossing_sections != std::make_pair(-1, -1))
+			{
 				return crossing_sections;
+			}
 		}
 		else
 		{
 			std::pair<int, int> crossing_sections =
-				DeleteSegment(i, segments, Segment_sort, skanline);
+				DeleteSegment(i, segments, Segment_sort, scanline);
 			if (crossing_sections != std::make_pair(-1, -1))
+			{
 				return crossing_sections;
+			}
 		}
 	}
 	return std::make_pair(-1, -1);
@@ -250,15 +278,23 @@ int main()
 	{
 		fin >> sec;
 		if (sec.left_end_.x_ > sec.right_end.x_)
+		{
 			sec = Segment(sec.right_end, sec.left_end_);
+		}
 		segments.push_back(sec);
 	}
 	std::pair<int, int> answer = FindCrossSegments(segments);
 	if (answer.first > answer.second)
-		answer = std::make_pair(answer.second, answer.first);
+	{
+        std::swap(answer.second, answer.first);
+	}
 	if (answer == std::make_pair(-1, -1))
+	{
 		std::cout << "NO";
+	}
 	else
+	{
 		std::cout << "YES\n" << answer.first + 1 << ' ' << answer.second + 1;
+	}
 	return 0;
 }
